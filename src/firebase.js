f8862da68310c1631541b355ea3dee3548adc319
Keith Austria <austriakeith08@gmail.com>
2026-05-07 10:52:08 +0800

    fix: use env variables for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/firebase-app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNetx1pKEkik1DFMX04CJepavS4EdHbjs",
-  authDomain: "barangay-complaint-syste-c6310.firebaseapp.com",
-  projectId: "barangay-complaint-syste-c6310",
-  storageBucket: "barangay-complaint-syste-c6310.firebasestorage.app",
-  messagingSenderId: "218754104679",
-  appId: "1:218754104679:web:f57380dc02bcef7e3f83c2",
-  measurementId: "G-XFY4F4QGX0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
